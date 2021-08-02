6b1f2ffef89bb071159328a5d2a95b7de8c3e145
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-02 14:40:23 -0400

    Cleaning up code to start over

--- Program.cs
+++ Program.cs
@@ -5,68 +5,24 @@ namespace BlackJackCS
 {
   class Program
   {
-    public static void OpeningGreeting()
+    public static string OpeningGreeting()
     {   
       Console.WriteLine("Welcome to Black Jack\n");
       Console.Write("What is your name? ");
       var name = Console.ReadLine();
-      Console.WriteLine($"It is a pleasure to meet you {name}! Press any key to begin.");
+      Console.WriteLine($"Hi {name}, press any key to begin.");
       Console.ReadLine();
-    }
-    public static List<string> DeckCreationAndShuffle()
-    {
-      var suits = new List<string>() { "Hearts", "Spades", "Clubs", "Diamonds", };
-      var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", };
-      var deckOfCards = new List<string>();
-      foreach (var suit in suits)
-      {
-        foreach (var rank in ranks)
-        {
-            var card = $"{rank} of {suit}";
-            deckOfCards.Add(card);
- 
-        }
+      Console.Write("");
       
-        return deckOfCards; 
-
-      }
+      return name;
+    }
     
-      var numberOfCards = deckOfCards.Count;
- 
-      for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
-        {
-          var randomNumberGenerator = new Random();
-          var leftIndex = randomNumberGenerator.Next(rightIndex);
-            
-          var leftCard = deckOfCards[leftIndex];
-          var rightCard = deckOfCards[rightIndex];
-            
-          deckOfCards[rightIndex] = leftCard;            
-          deckOfCards[leftIndex] = rightCard;
-        }
-        
-      } 
     
     static void Main(string[] args)
     {
         OpeningGreeting();
 
-        Console.WriteLine("Let's begin!\n");
-
-        string myDeckOfCards = DeckCreationAndShuffle();
-
-        string playerFirstCard = myDeckOfCards[0];
-        Console.WriteLine($"Player's first card is: {playerFirstCard}");
- 
-        string playersecondCard = myDeckOfCards[2];
-        Console.WriteLine($"Player's second card is: {playersecondCard}");
-
-        string dealerFirstCard = myDeckOfCards[1];
-        Console.WriteLine($"Dealer's first card is: {dealerFirstCard}");
- 
-        string dealerSecondCard = myDeckOfCards[3];
-        Console.WriteLine($"Dealer's second card is: (Face Down)");
-
+        Console.WriteLine("Let's begin!\n"); 
     }
   }
 }
